40ab9f118e630bef27af20ea1a83c6bdd5380f57
Amber <amberaltenburg@gmail.com>
2016-01-20 14:57:42 +0100

    tui

--- src/qwirkle/TUIView.java
+++ src/qwirkle/TUIView.java
@@ -7,15 +7,28 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class TUIView implements View {
-	
+
 	private QwirkleClient client;
 	private Game game;
-	
+
 	public TUIView(QwirkleClient client) {
 		this.client = client;
 	}
-		
+
 	public void run() {
+		// Create scanner to obtain user input
+		Scanner user_input = new Scanner(System.in);
+		String playerName;
+		// Obtain user input
+		System.out.print("Enter your name: ");
+		playerName = user_input.nextLine();
+		// Output information
+		System.out.print("Your name is: " + playerName);
+		client.requestJoin(playerName);
+		
+		//String playerCount;
+		//System.out.print("");
+
 		try (Scanner scanner = new Scanner(System.in)) {
 			boolean continueLoop = true;
 			while (continueLoop) {
@@ -24,7 +37,7 @@ public class TUIView implements View {
 			}
 		}
 	}
-	
+
 	private boolean processCommand(String command) {
 
 		boolean continueLoop = true;
@@ -45,39 +58,48 @@ public class TUIView implements View {
 	}
 
 	private void handleTradeCommand(Scanner commandScanner) {
-		// TODO Auto-generated method stub
-		
+		List<Stone> stones = new ArrayList<Stone>();
+
+		// trade <aantal stenen>
+		int stonesCount = Integer.parseInt(commandScanner.next());
+
+		for (int i = 0; i < stonesCount; i++) {
+			int stoneIndex = Integer.parseInt(commandScanner.next());
+		}
+
+		// TODO: ask controller for hand and retrieve stones from there.
+
 	}
 
 	private void handlePlaceCommand(Scanner commandScanner) {
 		List<Move> moves = new ArrayList<Move>();
-		
+
 		// place <aantal stenen> {<index> <x> <y> ....}
 		int stonesCount = Integer.parseInt(commandScanner.next());
-		
-		for (int i = 0; i < stonesCount; i++) { 
+
+		for (int i = 0; i < stonesCount; i++) {
 			int stoneIndex = Integer.parseInt(commandScanner.next());
 			int x = Integer.parseInt(commandScanner.next());
 			int y = Integer.parseInt(commandScanner.next());
 			Location location = new Location(x, y);
-			
+
 			// TODO: ask controller for hand and retrieve stones from there.
-			
+
 			Move move = new Move(null, location);
 			moves.add(move);
 		}
-		
+
 		client.setMove(moves);
 		System.out.println(moves.size());
 	}
 
 	public void updateView() {
-		
+
 	}
 
 	@Override
 	public void buildView() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
